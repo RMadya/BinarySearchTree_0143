19a0e2c01b8e0558d0b4384e22ab1a826024a200
RMadya <kereyoben75@gmail.com>
2024-06-14 10:17:54 +0700

    kasus 1 untuk enter word

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -122,4 +122,14 @@ int main()
 		cin >> ch;
 		cout << endl;
 
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			x.insert(word);
+			break;
+		}
 		
