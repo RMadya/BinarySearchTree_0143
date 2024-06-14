7001495bfac12e4f47bf07ab45b4d22b83c9c48d
RMadya <kereyoben75@gmail.com>
2024-06-14 09:52:57 +0700

    deklarasi class node dan public string

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -5,13 +5,13 @@ using namespace std;
 class Node {
 public:
 	string info;
-	Node* leftchild;
-	Node* rightchild;
+	Node* Leftchild;
+	Node* Rightchild;
 
 	Node(string i, Node* l, Node* r) {
 		info = i;
-		leftchild = l;
-		rightchild = r;
+		Leftchild = l;
+		Rightchild = r;
 	}
 };
 
